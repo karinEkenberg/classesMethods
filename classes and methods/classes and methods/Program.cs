e9c6efa6995dea9b48651e7430a7a4f8b38982ac
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2023-11-29 17:03:04 +0100

    Add files via upload

---
+++ classes and methods/classes and methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+            Car myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "800";
+            myCar.Year = 1986;
+            myCar.Color = "Blue";
+            Console.WriteLine( " {0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+            Console.ReadLine();
+        }
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+
+        }
+    }
+    class Car {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            return carValue;
+        }
+  
+
+    }
+
+}
